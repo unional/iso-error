2f5a153f5702d4bbcffec73114082482614d30bd
Homa Wong (unional) <homawong@gmail.com>
2019-09-12 14:25:32 -0700

    test: add missing test

--- src/fromErrorStatus.spec.ts
+++ src/fromErrorStatus.spec.ts
@@ -1,5 +1,6 @@
 import { Aborted, AlreadyExists, Cancelled, DataLoss, DeadlineExceeded, FailedPrecondition, fromErrorStatus, InternalError, InvalidArgument, NotFound, OutOfRange, PermissionDenied, ResourceExhausted, Unauthenticated, Unavailable, Unimplemented, UnknownError } from '.'
 import { getStatusExample } from './test-utils'
+import { GoogleCloudApiError } from './errors'
 test.each([
   [1, 'Cancelled', Cancelled],
   [3, 'InvalidArgument', InvalidArgument],
@@ -19,4 +20,12 @@ test.each([
   [16, 'Unauthenticated', Unauthenticated],
 ])('status %d converts to Error %s', (code, _name, c) => {
   expect(fromErrorStatus(getStatusExample(code as number)!)).toBeInstanceOf(c)
+})
+
+test('unknown code converts to base GoogleCloudApiError', () => {
+  expect(fromErrorStatus({
+    code: 123,
+    message: '',
+    details: []
+  })).toBeInstanceOf(GoogleCloudApiError)
 })
